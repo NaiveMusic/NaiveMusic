e34eb097190dcde58abeb417eeb4a519f59f0a11
zhangfeiran <feiranzhang@outlook.com>
2018-11-24 16:10:50 +0800

    update demo with pyqt5

--- Demo/demo.py
+++ Demo/demo.py
@@ -1,11 +1,64 @@
 import subprocess
-import msvcrt
+import sys
+from PyQt5 import QtCore, QtWidgets, uic
 
-fs = subprocess.Popen(['fluidsynth.exe', '-n', 'GeneralUser_GS.sf2'], stdin=subprocess.PIPE, bufsize=0)
+Ui_MainWindow, QtBaseClass = uic.loadUiType('./demo.ui')
 
-pitch = [58, 60, 62, 64, 65, 67, 69, 71, 72]
-while True:
-    a = msvcrt.getch()
-    print(int(a))
-    a = 'noteon 0 ' + str(pitch[int(a)]) + ' 100'
-    fs.stdin.write(bytes(a + '\r\n', encoding='utf8'))
+
+class Demo(QtWidgets.QMainWindow, Ui_MainWindow):
+    def __init__(self):
+        QtWidgets.QMainWindow.__init__(self)
+        Ui_MainWindow.__init__(self)
+        self.setupUi(self)
+        self.setWindowTitle('Demo')
+
+        for line in open('instrument.txt'):
+            self.instrument.addItem(line.strip())
+        self.instrument.currentIndexChanged.connect(self.ChangeInst)
+        self.pitchButtons = QtWidgets.QButtonGroup()
+        for i in range(8):
+            cur = QtWidgets.QPushButton()
+            cur.setText(str(i + 1))
+            cur.setObjectName(str(i + 1))
+            self.verticalLayout.addWidget(cur)
+            self.pitchButtons.addButton(cur)
+        self.pitchButtons.buttonClicked.connect(self.MousePlay)
+        self.play.clicked.connect(self.MusicPlay)
+
+        self.fs = subprocess.Popen(['fluidsynth.exe', '-n', 'GeneralUser_GS.sf2'], stdin=subprocess.PIPE, bufsize=0)
+        self.pitch = ['58', '60', '62', '64', '65', '67', '69', '71', '72']
+
+    def send(self, cmd):
+        self.fs.stdin.write(bytes(cmd + '\r\n', encoding='utf8'))
+
+    def MusicPlay(self):
+        music = self.music.toPlainText()
+        tempo = self.tempo.value()
+        for i in music:
+            if i == ' ':
+                self.send('sleep ' + str(tempo))
+            elif int(i) > 0 and int(i) < 9:
+                self.send('noteon 1 ' + self.pitch[int(i)] + ' 100')
+                self.send('sleep ' + str(tempo))
+                self.send('noteoff 1 ' + self.pitch[int(i)])
+
+    def MousePlay(self, button):
+        note = int(button.objectName())
+        print(note)
+        self.send('noteon 1 ' + self.pitch[note] + ' 100')
+
+    def ChangeInst(self):
+        inst = self.instrument.currentIndex()
+        self.send('prog 1 ' + str(inst))
+
+    def keyPressEvent(self, event):
+        note = event.key() - 48
+        if note > 0 and note < 9:
+            print(note)
+            self.send('noteon 1 ' + self.pitch[note] + ' 100')
+
+
+app = QtWidgets.QApplication(sys.argv)
+window = Demo()
+window.show()
+sys.exit(app.exec_())
--- Demo/tax_calc.py
+++ Demo/tax_calc.py
@@ -4,6 +4,7 @@ from PyQt5 import QtCore, QtWidgets, uic
 qtCreatorFile = "./tax_calc.ui"
 Ui_MainWindow, QtBaseClass = uic.loadUiType(qtCreatorFile)
 
+
 class MyApp(QtWidgets.QMainWindow, Ui_MainWindow):
     def __init__(self):
         QtWidgets.QMainWindow.__init__(self)
@@ -14,7 +15,7 @@ class MyApp(QtWidgets.QMainWindow, Ui_MainWindow):
     def CalculateTax(self):
         price = int(self.price_box.toPlainText())
         tax = (self.tax_rate.value())
-        total_price = price  + ((tax / 100) * price)
+        total_price = price + ((tax / 100) * price)
         total_price_string = "The total price with tax is: " + str(total_price)
         self.results_window.setText(total_price_string)
 
