78590834119dda7e24679ed0038b6e94371fbc00
Hydrilla <wuhx15@mails.tsinghua.edu.cn>
2018-12-09 22:02:25 +0800

    Add track/file manager part for controller

--- controller.py
+++ controller.py
@@ -2,6 +2,7 @@ from track import Track
 from note import Note
 from file import File
 from const import *
+import pickle
 
 ## TODO
 # ADD BPM,MPB in class File
@@ -9,26 +10,38 @@ from const import *
 # ADD keyDict in const.py
 # COMPLETE demo 2.0 using MVC
 
+# del mechanics
+
 
 class Controller():
 	def __init__(self):
 		self.__file = NULL
 		self.__track = NULL
-		self.__trackDict = {'viewID':'trackID'}
-		self.__cwTrackID = 0
-		self.__cwPos = 0
+		self.__trackDict = {} #{'viewID':'trackID'}
+		self.__cwTrackID = -1
+		self.__cwPos = -1
 		self.__defaultInst = 0
 		self.__defaultVel = 100
-		self.__states = 'DEFAULT'
+		self.__states = 'EMPTY'
 	
 	def createNewFile():
-		
+		self.__file = File()
+		self.__states = 'EDITABLE'
+
 	def saveFile():
-	
+		pickle.dump(__file, open("test.nm", "wb"))
+		
 	def loadFile():
+		self.__file = pickle.load(open("test.nm", "rb"))
+		self.__states = 'EDITABLE'
 	
 	def delFile():
-
+		del self.__file # file.destruct()? del track?
+		self.__track = NULL
+		self.__cwPos = -1
+		self.__cwTrackID = -1
+		self.__trackDict.clear()
+		self.__states = 'EMPTY'
 
 
 	def setWorkingTrack(viewID):
@@ -37,11 +50,13 @@ class Controller():
 		self.__cwPos = 0
 
 	def addTrack(viewID, inst=self.__defaultInst, vel=self.__defaultVel):
-		trackID = __file.addTrack(inst,vel) ##
+		# create and switch cwTrack to a new track
+		trackID = self.__file.addTrack(inst,vel)
 		self.__trackDict[viewID] = trackID
 		self.setWorkingTrack(viewID)
 
 	def delTrack(viewID, toViewID):
+		# delete track[viewID] and switch cwTrack to track[toViewID]
 		self.__file.delTrack(self.__trackDict[viewID])
 		del self.__trackDict[viewID]
 		self.setWorkingTrack(toViewID)
@@ -66,9 +81,30 @@ class Controller():
 		noteList = self.__track.search(key, on, off)
 		for noteID in noteList:
 			self.__track.delNote(noteID)
+		#...
 
-	def function():
+
+	def getTrackInfo(viewID = NULL):
+		if viewID == NULL:
+			track = self.__track
+		else
+			track = self.__file.getTrack(self.__trackDict[viewID])
+
+		inst, vel = track.getInfo()
+
+		trackInfo = {}
+		trackInfo['Instrument'] = inst;
+		trackInfo['Velocity'] = vel
+		
+		return trackInfo
+
+	def getFileInfo():
 		pass
+
+	def display():
+		pass
+
+		
 
 
 
