f0e0a2c6dd5cb087ec54af57e6ab293df322d5b3
Hydrilla <nitromewhx@live.com>
2018-12-14 23:43:17 +0800

    Update track.py
    Add search() method;
    Del getInfo() method

--- model/track.py
+++ model/track.py
@@ -11,10 +11,6 @@ class Track():
         self.vel = vel
         self.demoNotes = ''
 
-    def getInfo(self):
-        '''Return track instrument and velocity'''
-        return self.inst, self.vel
-
     def getNote(self, noteID):
         if noteID not in self.notes:
             raise ValueError('Note not found')
@@ -37,3 +33,32 @@ class Track():
             raise ValueError('Note not found')
         poped = self.notes.pop(noteID)
         del poped
+
+    def search(self, keys=KEY_RANGE, on, off):
+    """ Search for all notes satisfying that
+        1) note.key is in keys;
+        2) the interval [note.on, note.off) has non-empty intersection with [on,off).
+        Return a list of noteID
+
+    Args:
+        keys (<list>int): the range of 'key' property. Defaults to all keys.
+        on (int): the start time of the interval.
+        off(int): the end time of the interval.
+
+    P.S. we don't search on 'vel' property because it is not characteristic.
+    """
+        resultList = []
+        for noteID,note in self.notes:
+            if (note.key in keys && self.__intersect(on, off, note.on, note.off)):
+                 resultList.append(noteID)
+        return resultList
+
+    def __intersect(on1, off1, on2, off2):
+        """ Return boolen value for the condition
+            whether interval [on1,off1) intersects with [on2,off2)
+        """
+        if max(on1,off1) <= on2:
+            return False
+        if min(on1,off1) >= off2:
+            return False
+        return True
