b899225ab478eeefe3e5e91fe88a587ed3c19aa7
Hydrilla <nitromewhx@live.com>
2018-12-19 12:29:47 +0800

    ...

--- controller/audioController.py
+++ controller/audioController.py
@@ -1,6 +1,6 @@
 import os
 import subprocess
-
+import ctypes
 
 class AudioController():
     def __init__(self):
@@ -9,11 +9,15 @@ class AudioController():
         elif os.sys.platform.startswith('linux'):
             pass
         elif os.sys.platform.startswith('darwin'):
-            raise NotImplementedError('TODO for wu tong xue')
+            pass
+
 
     def _play(self):
         if os.sys.platform.startswith('win'):
             self.player.stdin.write(bytes('a', encoding='utf8'))
         elif os.sys.platform.startswith('linux'):
             os.system('fluidsynth -a pulseaudio -ni fluidsynth/GeneralUser_GS.sf2 fluidsynth/tmp.mid')
+        elif os.sys.platform.startswith('darwin'):
+            pass
+
         
