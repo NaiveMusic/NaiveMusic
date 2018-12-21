8edb709acaff1ff4a9809fb6a9ee946dee1a2800
siyuanSsun <thusiyuansun@gmail.com>
2018-12-21 19:55:20 +0800

    调整了一下trackView的layout

--- view/mainView.py
+++ view/mainView.py
@@ -5,29 +5,80 @@ from PyQt5.QtGui import *
 
 from .trackView import TrackView, InstrumentView
 from .sheetView import SheetView_Demo
-#from controller.mainController import MainController
+from controller.mainController import MainController
 
 
 class MainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
-
-
+        self.initUI()
+        self.isPlay = True
 
         self.vbox = QVBoxLayout()
-        self.vbox.addWidget(SheetView_Demo())
+        self.vbox.addWidget(self.sheet)
         self.vbox.addWidget(InstrumentView())
-        self.vbox.addWidget(TrackView(1))
-
+        hbox = QHBoxLayout()
+        hbox.addWidget(self.playButton)
+        hbox.addWidget(self.playAllButton)
+        self.vbox.addLayout(hbox)
+        self.vbox.addWidget(self.tracklist[0])
+        for track in self.tracklist:
+            self.vbox.addWidget(track)
         self.trackRegion = QWidget(self)
         self.trackRegion.setGeometry(QRect(100, 100, 1280, 720))
         self.trackRegion.setLayout(self.vbox)
         self.setWindowTitle('Naive Music')
-        self.resize(1280, 720)
+        self.resize(1920, 1080)
+
+
+    def initUI(self):
+        self.initToolUI()
+        self.initSheetUI()
+        self.initTrackUI()
+
+    def initToolUI(self):
+        self.playButton = QPushButton('', self)
+        self.playButton.setMinimumSize(80, 80)
+        self.playButton.setIcon(QIcon('view/Icons/ui/play.svg'))
+        self.playButton.clicked.connect(self.playTrack)
+
+        self.playAllButton = QPushButton('', self)
+        self.playAllButton.setMinimumSize(80, 80)
+        self.playAllButton.setIcon(QIcon('view/Icons/ui/playAll.svg'))
+        self.playAllButton.clicked.connect(self.playAll)
+
+    def initSheetUI(self):
+        self.sheet = SheetView_Demo()
+        self.sheet.textChanged.connect(self.updateTrack)
+
+    def initTrackUI(self):
+        self.mc = MainController()
+        self.tracklist = []
+        for i in range(2):
+            trackID = self.mc.curFile.addTrack(inst=0, vel=100)
+            self.tracklist.append(TrackView(self.mc, trackID, self.sheet))
+
+    def playTrack(self):
+        self.mc.curFile.setBPM(100)
+        if self.isPlay:
+            self.playButton.setIcon(QIcon('view/Icons/ui/pause.svg'))
+            self.isPlay = False
+        else: 
+            self.playButton.setIcon(QIcon('view/Icons/ui/play.svg'))
+            self.isPlay = True
+
+        self.mc.playTrack(self.mc.curTrackID)
+
+    def playAll(self):
+        self.mc.playAll()
 
     def addTrack(self):
+
         pass
 
+    def updateTrack(self):
+        text = self.sheet.toPlainText()
+        self.mc.curTrack.demoNotes = text
 '''
 class MainWindow_Demo(QtWidgets.QMainWindow):
     def __init__(self):
@@ -50,7 +101,7 @@ class MainWindow_Demo(QtWidgets.QMainWindow):
         # 全局播放
         self.play = QtWidgets.QPushButton()
         self.play.setText('Play All')
-        self.play.clicked.connect(self.playAll)
+        self.play.clicked.connect(self.playAllButton)
         self.grid.addWidget(self.play, 1, 0)
 
         # bpm label
@@ -114,7 +165,7 @@ class MainWindow_Demo(QtWidgets.QMainWindow):
         self.mc.curFile.setBPM(self.bpm.value())
 
     # 全局播放
-    def playAll(self):
+    def playAllButton(self):
         self.mc.playAll()
 
     # 切换track
--- view/trackView.py
+++ view/trackView.py
@@ -6,31 +6,32 @@ from model.const import *
 from controller.mainController import MainController
 
 class InstrumentView(QWidget):
-	def __init__(self):
-		super().__init__()
-		self.initUI()
-		self.instrumentID = None
-		hbox = QHBoxLayout()
-		hbox.addWidget(self.instrumentLabel)
-		hbox.addWidget(self.cancelButton)
-		hbox.addStretch(1)
-		self.setLayout(hbox)
+    def __init__(self):
+        super().__init__()
+        self.initUI()
+        self.instrumentID = None
+        hbox = QHBoxLayout()
+        hbox.addWidget(self.instrumentLabel)
+        hbox.addWidget(self.cancelButton)
+        hbox.addStretch(1)
+        self.setLayout(hbox)
 
-	def initUI(self):
-		self.instrumentLabel = QLabel('Default', self)
-		self.instrumentLabel.setMinimumSize(50, 50)
-		self.cancelButton = QPushButton('', self)
-		self.cancelButton.setMinimumSize(10, 10)
-		self.cancelButton.setIcon(QIcon('view/Icons/ui/cancel.svg'))
-		self.cancelButton.clicked.connect(self.deleteInstrument)
+    def initUI(self):
+        self.instrumentLabel = QLabel('Default', self)
+        self.instrumentLabel.setMinimumSize(50, 50)
+        self.cancelButton = QPushButton('', self)
+        self.cancelButton.setMinimumSize(10, 10)
+        self.cancelButton.setIcon(QIcon('view/Icons/ui/cancel.svg'))
+        self.cancelButton.clicked.connect(self.deleteInstrument)
 
 
-	def deleteInstrument(self):
-		self.deleteLater()
+    def deleteInstrument(self):
+        self.deleteLater()
 
 class TrackView(QWidget):
-    def __init__(self, trackController, trackID):
+    def __init__(self, trackController, trackID, sheet):
         super().__init__()
+        self.sheet = sheet
         self.trackID = trackID
         self.trackController = trackController
         self.value = 0
@@ -66,9 +67,9 @@ class TrackView(QWidget):
 
     # 初始化音轨区域
     def initTrackUI(self):
-    	self.track = QPushButton('track', self)
-    	self.track.setMinimumSize(1160, 120)
-    	# self.track.clicked.connect()
+        self.track = QPushButton('track', self)
+        self.track.setMinimumSize(1160, 120)
+        # self.track.clicked.connect(self.playTrack)
 
 
     # 轨道乐器图标
@@ -79,65 +80,67 @@ class TrackView(QWidget):
         self.instrumentButton.setIcon(QIcon('view/Icons/instrument/default.svg'))
 
     def bindInstrument(self, icon, instID):
-    	self.instrumentButton.setIcon(icon)
-    	self.trackController.curFile.getTrack(self.trackID).setInst(instID)
+        self.instrumentButton.setIcon(icon)
+        self.trackController.curFile.getTrack(self.trackID).setInst(instID)
         
 
     # 音量图标界面初始化函数
     def initVolumeUI(self):
-    	# 音量按钮，点击效果静音，随着音量的变化，图标进行变化
-    	self.volume = QPushButton('', self)
-    	self.volume.setMinimumSize(30, 30)
-    	self.volume.setIcon(QIcon('view/Icons/volume/midVolume.svg'))
-    	self.currentVIcon = QIcon('view/Icons/volume/midVolume.svg')
-    	self.volume.clicked.connect(self.muteTrack)
+        # 音量按钮，点击效果静音，随着音量的变化，图标进行变化
+        self.volume = QPushButton('', self)
+        self.volume.setMinimumSize(30, 30)
+        self.volume.setIcon(QIcon('view/Icons/volume/midVolume.svg'))
+        self.currentVIcon = QIcon('view/Icons/volume/midVolume.svg')
+        self.volume.clicked.connect(self.muteTrack)
 
-    	# 音量滑动条
-    	self.vAdjuster = QSlider(Qt.Horizontal, self)
-    	self.vAdjuster.setMinimumSize(50, 30)
-    	self.vAdjuster.setMaximum(100)
-    	self.vAdjuster.setValue(50)
-    	self.vAdjuster.valueChanged[int].connect(self.changeValue)
+        # 音量滑动条
+        self.vAdjuster = QSlider(Qt.Horizontal, self)
+        self.vAdjuster.setMinimumSize(50, 30)
+        self.vAdjuster.setMaximum(100)
+        self.vAdjuster.setValue(50)
+        self.vAdjuster.valueChanged[int].connect(self.changeValue)
 
 
-    # 音量滑动条连接函数
+    # 音量滑动条连接函数         
     def changeValue(self, value):
-    	self.value = value
-    	print(self.value)
-    	if value == 0:
-    		self.volume.setIcon(QIcon('view/Icons/volume/zeroVolume.svg'))
-    		self.currentVIcon = QIcon('view/Icons/volume/zeroVolume.svg')
-    	elif value > 0 and value <= 30:
-    		self.volume.setIcon(QIcon('view/Icons/volume/minVolume.svg'))
-    		self.currentVIcon = QIcon('view/Icons/volume/minVolume.svg')
-    	elif value > 30 and value <= 80:
-    		self.volume.setIcon(QIcon('view/Icons/volume/midVolume.svg'))
-    		self.currentVIcon = QIcon('view/Icons/volume/midVolume.svg')
-    	else:
-    		self.volume.setIcon(QIcon('view/Icons/volume/maxVolume.svg'))
-    		self.currentVIcon = QIcon('view/Icons/volume/maxVolume.svg')
+
+        self.value = value
+        self.trackController.curFile.getTrack(self.trackID).setVel(self.value)
+        if value == 0:
+            self.volume.setIcon(QIcon('view/Icons/volume/zeroVolume.svg'))
+            self.currentVIcon = QIcon('view/Icons/volume/zeroVolume.svg')
+        elif value > 0 and value <= 30:
+            self.volume.setIcon(QIcon('view/Icons/volume/minVolume.svg'))
+            self.currentVIcon = QIcon('view/Icons/volume/minVolume.svg')
+        elif value > 30 and value <= 80:
+            self.volume.setIcon(QIcon('view/Icons/volume/midVolume.svg'))
+            self.currentVIcon = QIcon('view/Icons/volume/midVolume.svg')
+        else:
+            self.volume.setIcon(QIcon('view/Icons/volume/maxVolume.svg'))
+            self.currentVIcon = QIcon('view/Icons/volume/maxVolume.svg')
 
     # 音轨静音效果
     def muteTrack(self):
-    	# 
-    	if self.isMute: 
-    		self.volume.setIcon(QIcon('view/Icons/volume/mute.svg'))
-    		self.value = 0
-    		self.isMute = False
-    	else:
-    		self.volume.setIcon(self.currentVIcon)
-    		self.value = self.vAdjuster.value()
-    		self.isMute = True
+        # 
+        if self.isMute: 
+            self.volume.setIcon(QIcon('view/Icons/volume/mute.svg'))
+            self.value = 0
+            self.isMute = False
+        else:
+            self.volume.setIcon(self.currentVIcon)
+            self.value = self.vAdjuster.value()
+            self.isMute = True
 
-    
+        self.trackController.curFile.getTrack(self.trackID).setVel(self.value)
 
-    def playTrack(self):
-    	self.trackController.playTrack(self.trackID)
-        pass
+
+    def selectTrack(self):
+        self.trackController.setCurTrack(self.trackID)
+        self.sheet.setPlainText(self.trackController.curTrack.demoNotes)
 
     def deleteTrack(self):
-    	# 删除音轨暂时不需要实现
-    	pass
+        # 删除音轨暂时不需要实现
+        pass
 
 '''
 
